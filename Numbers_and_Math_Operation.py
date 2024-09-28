368ed92a26152c87630a8c20d71991d2988718ae
Rakesh-TN <tnrakesh68@outlook.com>
2024-09-28 22:53:18 +0530

    first commit

---
+++ Numbers_and_Math_Operation.py
@@ -0,0 +1,26 @@
+# Numbers and Math Operations
+
+print('Data Types - Int, Float, Complex')
+
+x = 1
+y = 2.0
+z = 3j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+a = 160
+b = 40
+
+"""
+Addition - +
+Subtraction - -
+Multiplication - *
+Division - /
+Modulus - %
+Exponential - **
+Floor Division - //
+"""
+
+print(a*b)
